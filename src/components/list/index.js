a9a5ab5273a9386f64b763900605db67c611626c
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2019-08-06 19:35:02 +0800

    bug fix

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from "react-redux";
 import './style.scss'
-import ReactList from 'react-list';
 import { getOrders } from '../../redux/ducks/orders'
 import { viewData } from '../../redux/ducks/modalForm'
 import { cancelOrder } from '../../redux/ducks/order';
@@ -10,45 +9,43 @@ import { cancelOrder } from '../../redux/ducks/order';
 class List extends React.Component { // eslint-disable-line react/prefer-stateless-function
     componentDidMount() {
         getOrders();
-        setInterval(getOrders, 5000);
+        setInterval(getOrders, 2000);
     }
 
-    renderItem = (index, key) => {
-        const { isCancelling, orders } = this.props;
-        const data = orders[index];
+    renderRows = (data, index) => {
+        const { isCancelling } = this.props;
         const isCancelled = data.state === 'cancelled';
         const isDelivered = data.state === 'delivered';
         return (
-            <div className={`cell ${index % 2 && 'cell-odd'}`} key={key}>
-                <div>
-                    {index + 1}
-                </div>
-                <div>
+            <tr className={`row ${index % 2 && 'row-odd'}`} key={index}>
+                <td className="cell">
+                    {data.id}
+                </td>
+                <td className="cell">
                     {data.item_name}
-                </div>
-                <div>
+                </td>
+                <td className="cell">
                     RM {(data.price / 100).toFixed(2)}
-                </div>
-                <div className={`cell-${data.state}`}>
+                </td>
+                <td className={`cell cell-${data.state}`}>
                     {data.state}
-                </div>
-                <div>
+                </td>
+                <td className="cell">
                     <button onClick={() => viewData(data)}>View</button>
                     {!(isCancelled || isCancelling || isDelivered) && <button onClick={() => cancelOrder(data.id)}>Cancel</button>}
                     {isCancelling && '.....'}
-                </div>
-            </div>
-        );
+                </td>
+            </tr>);
     }
 
     render() {
         return (
             <div className='container'>
-                <ReactList
-                    itemRenderer={this.renderItem}
-                    length={this.props.orders.length}
-                    type='uniform'
-                />
+                <table className="w-100">
+                    <tbody>
+                        {this.props.orders.map(this.renderRows)}
+                    </tbody>
+                </table>
             </div>
         );
     }
